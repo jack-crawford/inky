dfdbec9c671cb657b5d28a80390101478ac1427b
Jack <jackcrawford2008@gmail.com>
2018-03-09 16:22:19 -0600

    Initial commit

---
+++ inky/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  inky
+//
+//  Created by Jack Crawford on 3/9/18.
+//  Copyright © 2018 tangle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
